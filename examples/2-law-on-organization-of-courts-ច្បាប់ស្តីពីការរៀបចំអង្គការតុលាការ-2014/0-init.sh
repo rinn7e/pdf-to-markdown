eb4c2acf7b8d785011f162ecc7ea7022d761bce9
Moremi Vannak <moremi.va@gmail.com>
2026-03-22 15:14:11 +0700

    add init script for examples

---
+++ examples/2-law-on-organization-of-courts-ច្បាប់ស្តីពីការរៀបចំអង្គការតុលាការ-2014/0-init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create output directories for each stage of the toolkit
+mkdir -p 1-output-images
+mkdir -p 2-output-markdown
+mkdir -p 3-clean-markdown
+mkdir -p 4-en-markdown
+mkdir -p 5-combine-markdown
